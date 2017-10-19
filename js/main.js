d5abadabe5920745c2ff5c36fe402bbc76df3893
Leandro Camargo <leandroico@gmail.com>
2017-10-19 01:29:35 -0200

    Create birthday form with the correct workflow and UX

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 !function($) {
   $('select').selectBoxIt();
+  $('.registration-types .toggle').on('click', switchForms);
+  $('.fake-date-field').on('focus', enableRealDateField);
+  $('.fake-date-field + input').on('blur', getBackToFakeDateField);
+
+  function switchForms(ev) {
+    var $self = $(this), nextScreen;
+    ev.preventDefault();
+
+    if (!(nextScreen = $self.data('goto'))) return;
+    $self.closest('.active').removeClass('active');
+    $('#' + nextScreen).addClass('active');
+  }
+
+  function enableRealDateField(ev) {
+    ev.preventDefault();
+    $(this).attr('disabled', true);
+    $(this).next().trigger('focus');
+  }
+
+  function getBackToFakeDateField(ev) {
+    var $dateField = $(this);
+    if (!$dateField.val()) $dateField.prev().removeAttr('disabled');
+  }
 }(jQuery);
